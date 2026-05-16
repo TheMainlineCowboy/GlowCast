509e39340dab8686e29e77a24cdb35b3ebcf809e
TheMainlineCowboy <theaacowboy@gmail.com>
2026-05-15 18:49:32 -0700

    Load shape guide styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../styles.css";
 import "./animation-upgrades.css";
+import "./shape-guides.css";
 import "./effect-controls";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
